e472ec88236735e774761192a52d7690735436bb
J Yapp <yehbebe0112@gmail.com>
2022-08-09 12:34:07 +0200

    adding btns

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
+  state = {
+    count: 4,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div className="counter">
+        <span className={this.getBadgeClasses}>{this.formatCount}</span>
+        <button className="btn bg-secondary btn-sm">+</button>
+      </div>
+    );
+  }
+
+  get getBadgeClasses() {
+    let classes = "badge m-3 ";
+    return (classes += this.state.count === 0 ? "bg-warning" : "bg-primary");
+  }
+
+  get formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
